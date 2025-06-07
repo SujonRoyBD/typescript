51956434b183fb5157410e8a52b708ec3aedc5d7
SujonRoyBD <sujonroy537527@gmail.com>
2025-06-07 19:39:10 +0600

    this css change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/shared/Navbar/Navbar";
 import Footer from "@/shared/Footer/Footer";
+import { Navbar } from "@/shared/Navbar/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
